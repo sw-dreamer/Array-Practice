13955af1ccf1820397123ef99730cc3f1b666242
sw-dreamer <sw.woo.dreamer@gmail.com>
2024-12-06 18:57:21 +0900

    Update thirteenth_practice.java

--- 13. thirteen exercise/thirteenth_practice.java
+++ 13. thirteen exercise/thirteenth_practice.java
@@ -17,11 +17,11 @@ public class thirteenth_practice {
         Random random = new Random();
 //        배열 초기화
         int intArray[] = new int[10];
-        boolean duplicate = false; // 중복 체크        
+        boolean duplicate = false; // 중복 체크    ( 중복 되면 true, 중복되지 않으면 false)    
         int count = 0; 
         while (count < intArray.length) {
             int number = random.nextInt(10) + 1;
-            duplicate = false; // 중복 체크 초기화 ( 중복 되면 true, 중복되지 않으면 false)
+            duplicate = false; // 중복 체크 초기화 
             for (int i = 0; i < count; i++) {
                 // 중복 값이 존재하면 배열에 삽입을 못하게 함
                 if (intArray[i] == number) {
